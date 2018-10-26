2027e1f0f739121263181a45439cb24312b81e78
lbologna <9949990+lbologna@users.noreply.github.com>
2018-10-26 16:56:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ var header = {headers: {'Authorization': 'Bearer ' + session.access_token}};
 document.getElementById("hbp-token").innerHTML = session.access_token;
 console.log(header);
 //$.get(USER_API);
-$.get(USER_API, header);
-//$.getJSON(USER_API, header)
-//    .then(function (response) {
-//       resolve(response.data);
-//       console.log(response.data);
-// }); 
+//$.get(USER_API, header);
+$.getJSON(USER_API, header)
+    .then(function (response) {
+       resolve(response.data);
+       console.log(response.data);
+ }); 
   
  
  }); 
