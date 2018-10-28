b37e3442cf829c7e6c03d4930e331ba1d9424838
lbologna <9949990+lbologna@users.noreply.github.com>
2018-10-28 22:04:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,34 +15,27 @@ function init() {
 
 const USER_API = 'https://services.humanbrainproject.eu/idm/v1/api/user/me';
 var authorization = client.getToken();
+	
 console.log(authorization)
+	
 authorization.then((session) => {
 var header = {'headers' : {Authorization: 'Bearer ' + session.access_token}};
 document.getElementById("hbp-token").innerHTML = session.access_token;
+	
 console.log(header);
 	
-	    $.ajax({
-        url: USER_API,
-        headers: {
-            'Authorization':'Bearer ' + session.access_token,
-            'Content-Type':'application/json'
-        },
-        method: 'GET',
-        success: function(data){
-          console.log(data);
-        }
-      });
-	
-//$.get(USER_API);
-//$.get(USER_API, header);
-//$.getJSON(USER_API)
-//    .then(function (response) {
-//       resolve(response.data);
-//       console.log(response.data);
-// }); 
-  
- 
- }); 
+$.ajax({
+    url: USER_API,
+    headers: {
+        'Authorization':'Bearer ' + session.access_token,
+        'Content-Type':'application/json'
+    },
+    method: 'GET',
+    success: function(data){
+        document.getElementById("hbp-user-id").innerHTML = data;
+    }
+});
+}); 
 	 return authorization;
 }
 		   
